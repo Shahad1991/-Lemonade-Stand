674c12394674cd35c919026001ba339163c72afa
Shahad Al_ jawashee <shahad1nazar@gmail.com>
2025-03-07 12:53:57 +0100

    opgave del 3

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import LemonadeStand from "./LemonadeStand";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import Cart from './pages/Cart';
+import NavBar from './components/NavBar';
 
 function App() {
   
 
   return (
     <div className='App'>
-      <LemonadeStand />
+      <BrowserRouter>
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/shop" element={<Shop />} />
+                <Route path="/cart" element={<Cart />} />
+            </Routes>
+        </BrowserRouter>
     </div>
   );
 }
