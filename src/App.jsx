f3eb47bd5cf05cb1d783d2b746e26bf320f3bfb1
Shahad Al_ jawashee <shahad1nazar@gmail.com>
2025-03-11 12:14:55 +0100

    router changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,20 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Shop from './pages/Shop';
-import Cart from './pages/Cart';
-import NavBar from './components/NavBar';
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Shop from "./pages/Shop";
+import Cart from "./pages/Cart";
+import NavBar from "./components/NavBar";
 
 function App() {
-  
-
   return (
-    <div className='App'>
-      <BrowserRouter>
-            <NavBar />
-            <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/shop" element={<Shop />} />
-                <Route path="/cart" element={<Cart />} />
-            </Routes>
-        </BrowserRouter>
+    <div className="App">
+      <Router> {/* Skift fra BrowserRouter til HashRouter */}
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/shop" element={<Shop />} />
+          <Route path="/cart" element={<Cart />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
